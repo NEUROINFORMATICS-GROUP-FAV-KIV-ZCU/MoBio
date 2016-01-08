d5f45787784e1428d2e79a51b1f04eb93245e52e
Lubos <lubosmuller@seznam.cz>
2016-01-08 15:02:57 +0000

    activities screen - list with icons

--- www/js/controllers/activities.js
+++ www/js/controllers/activities.js
@@ -2,8 +2,10 @@ angular.module('mobio.controllers')
 
         .controller('ActivitiesCtrl', function ($scope) {
             $scope.activities = [
-                {title: 'Blood Pressure', id: 'bp'},
-                {title: 'Heart Rate', id: 'hr'}
+                {title: 'BP Monitor FORA', desc: 'sample description', id: 'bp', protocol: 'bts'},
+                {title: 'Heart Rate', desc: 'sample description', id: 'hr', protocol: 'antplus'},
+                {title: 'Blood Pressure', desc: 'sample description', id: 'bpant', protocol: 'antplus'},
+                {title: 'Weight Scale', desc: 'sample description', id: 'wgtant', protocol: 'antplus'}
             ];
         });
 
