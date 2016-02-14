7f89cb6fb6724e3c92c8b36fdaedafae7e35b3b8
Lubos <lubosmuller@seznam.cz>
2016-02-14 08:56:19 +0000

    odML BP refactored

--- www/js/services/odmlBloodPressureFora.js
+++ www/js/services/odmlBloodPressureFora.js
@@ -5,37 +5,44 @@ angular.module('mobio.odML')
             var odMLdataStructure = {
                 "odML": {
                     "author": "mobio_app",
-                    "date": "1970-01-01T00:00:00+00:00", //ISO 8601
+                    "date": "1970-01-01T00:00:00+00:00",
                     "version": 1,
                     "xmlns:gui": "http://www.g-node.org/guiml",
                     "section": [
-                        {
-                            "name": "blood_pressure",
-                            "type": "blood_pressure",
+                        {//0
+                            "name": "Blood Pressure Latest",
+                            "type": "bloodPressureLatest",
                             "property": [
-                                {
+                                {//0
+                                    "name": "date",
+                                    "value": {
+                                        "type": "string",
+                                        "content": "" //ISO 8601
+                                    }
+                                },
+                                {//1
                                     "name": "systolic",
                                     "value": {
                                         "type": "int",
                                         "content": 0
                                     }
                                 },
-                                {
+                                {//2
                                     "name": "diastolic",
                                     "value": {
                                         "type": "int",
                                         "content": 0
                                     }
                                 },
-                                {
+                                {//3
                                     "name": "mean",
                                     "value": {
                                         "type": "int",
                                         "content": 0
                                     }
                                 },
-                                {
-                                    "name": "heart_rate",
+                                {//4
+                                    "name": "heartRate",
                                     "value": {
                                         "type": "int",
                                         "content": 0
@@ -43,15 +50,42 @@ angular.module('mobio.odML')
                                 }
                             ]
                         },
-                        {
-                            "name": "device_info",
-                            "type": "device_info",
+                        {//1
+                            "name": "Blood Pressure Measurements",
+                            "type": "bloodPressureMeasurement",
+                            "section": [
+                            ]
+                        },
+                        {//2
+                            "name": "Defice Info",
+                            "type": "deviceInfo",
                             "property": [
-                                {
+                                {//0
                                     "name": "name",
                                     "value": {
                                         "type": "string",
-                                        "content": "FORA P30 Plus"
+                                        "content": ""
+                                    }
+                                },
+                                {//1
+                                    "name": "adress",
+                                    "value": {
+                                        "type": "string",
+                                        "content": ""
+                                    }
+                                },
+                                {//2
+                                    "name": "id",
+                                    "value": {
+                                        "type": "string",
+                                        "content": ""
+                                    }
+                                },
+                                {//3
+                                    "name": "class",
+                                    "value": {
+                                        "type": "int",
+                                        "content": 0
                                     }
                                 }
                             ]
@@ -62,46 +96,135 @@ angular.module('mobio.odML')
 
             return {
                 getBasicObject: function () {
-                    return odMLdataStructure;
+                    return JSON.parse(JSON.stringify(odMLdataStructure));
                 },
                 getDate: function (data) {
                     return data.odML.date;
                 },
-                getSystolic: function (data) {
-                    return data.odML.section[0].property[0].value.content;
+                getBloodPressureLatest: function (data) {
+                    return data.odML.section[0];
                 },
-                getDiastolic: function (data) {
-                    return data.odML.section[0].property[1].value.content;
+                getBloodPressureAll: function (data) {
+                    return data.odML.section[1];
                 },
-                getMean: function (data) {
-                    return data.odML.section[0].property[2].value.content;
+                getDeviceInfo: function (data) {
+                    return data.odML.section[2];
                 },
-                getHeartRate: function (data) {
-                    return data.odML.section[0].property[3].value.content;
+                getLatestHeartRate: function (data) {
+                    return data.odML.section[0].property[4].value.content;
                 },
                 setDate: function (data, value) {
                     var result = data;
                     result.odML.date = value;
                     return result;
                 },
-                setSystolic: function (data, value) {
+                setBloodPressureLatest: function (data, date, measurement) {
                     var result = data;
-                    result.odML.section[0].property[0].value.content = value;
+                    result.odML.section[0].property[0].value.content = date;
+                    result.odML.section[0].property[1].value.content = measurement.systolic;
+                    result.odML.section[0].property[2].value.content = measurement.diastolic;
+                    result.odML.section[0].property[3].value.content = measurement.mean;
+                    result.odML.section[0].property[4].value.content = measurement.heartRate;
                     return result;
                 },
-                setDiastolic: function (data, value) {
+                addBloodPressureMeasurement: function (data, date, measurement) {
+                    var toPush = {
+                        "name": "Blood Pressure",
+                        "type": "bloodPressure",
+                        "property": [
+                            {//0
+                                "name": "date",
+                                "value": {
+                                    "type": "string",
+                                    "content": date //ISO 8601
+                                }
+                            },
+                            {//1
+                                "name": "systolic",
+                                "value": {
+                                    "type": "int",
+                                    "content": measurement.systolic
+                                }
+                            },
+                            {//2
+                                "name": "diastolic",
+                                "value": {
+                                    "type": "int",
+                                    "content": measurement.diastolic
+                                }
+                            },
+                            {//3
+                                "name": "mean",
+                                "value": {
+                                    "type": "int",
+                                    "content": measurement.mean
+                                }
+                            },
+                            {//4
+                                "name": "heartRate",
+                                "value": {
+                                    "type": "int",
+                                    "content": measurement.heartRate
+                                }
+                            }
+                        ]
+                    };
                     var result = data;
-                    result.odML.section[0].property[1].value.content = value;
+                    result.odML.section[1].section.push(toPush);
                     return result;
                 },
-                setMean: function (data, value) {
+                resetBloodPressureMeasurement: function (data) {
                     var result = data;
-                    result.odML.section[0].property[2].value.content = value;
+                    result.odML.section[1].section = [];
                     return result;
                 },
-                setHeartRate: function (data, value) {
+                resetBloodPressureLatest: function (data) {
                     var result = data;
-                    result.odML.section[0].property[3].value.content = value;
+                    result.odML.section[0].property = [
+                        {//0
+                            "name": "date",
+                            "value": {
+                                "type": "string",
+                                "content": "" //ISO 8601
+                            }
+                        },
+                        {//1
+                            "name": "systolic",
+                            "value": {
+                                "type": "int",
+                                "content": 0
+                            }
+                        },
+                        {//2
+                            "name": "diastolic",
+                            "value": {
+                                "type": "int",
+                                "content": 0
+                            }
+                        },
+                        {//3
+                            "name": "mean",
+                            "value": {
+                                "type": "int",
+                                "content": 0
+                            }
+                        },
+                        {//4
+                            "name": "heartRate",
+                            "value": {
+                                "type": "int",
+                                "content": 0
+                            }
+                        }
+                    ];
+                    return result;
+                },
+                setDeviceInfo: function (data, deviceInfo) {
+                    var result = data;
+                    result.odML.section[2].property[0].value.content = deviceInfo.name;
+                    result.odML.section[2].property[1].value.content = deviceInfo.address;
+                    result.odML.section[2].property[2].value.content = deviceInfo.id;
+                    result.odML.section[2].property[3].value.content = deviceInfo.class;
                     return result;
                 }
             };
