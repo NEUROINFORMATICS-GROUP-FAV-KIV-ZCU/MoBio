bd65601eeab37a1f85c3cfd1bef1224ed1a25937
Lubos <lubosmuller@seznam.cz>
2016-03-28 11:50:50 +0100

    loading experiments spinner + error message

--- www/js/controllers/experimentList.js
+++ www/js/controllers/experimentList.js
@@ -2,29 +2,45 @@ angular.module('mobio.controllers')
 
         .controller('ExperimentListCtrl', function ($scope, $rootScope, $state, $ionicPopup, experimentService, experimentCache) {
 
+            $scope.showLoadingSpinner = false;
             $rootScope.global.experimentList = experimentCache.getExperiments().experiments;
             $rootScope.$on('experiment-added', function (event, args) {
                 $rootScope.global.experimentList = experimentCache.getExperiments().experiments;
             });
-            $scope.$on('$ionicView.enter', function(e) {
+            $scope.$on('$ionicView.enter', function (e) {
                 $rootScope.global.experimentList = experimentCache.getExperiments().experiments;
-            });  
-            
+            });
+
             $scope.setSelectedExperiment = function (experiment) {
                 experimentCache.setSelectedExperiment(experiment);
                 $rootScope.$emit('experiment-changed');
             };
-            
+
             $scope.getSelectedExperiment = function () {
                 return experimentCache.getSelectedExperiment();
             };
-            
-            $scope.syncExperiments = function() {
-                experimentService.getMyExperiments().then(function(response) {                    
+
+            $scope.syncExperiments = function () {
+                $scope.showLoadingSpinner = true;
+                experimentService.getMyExperiments().then(function (response) {
                     experimentCache.setExperiments(response.data.experimentDataList.experiments);
                     $rootScope.global.experimentList = experimentCache.getExperiments().experiments;
                     $rootScope.$emit('experiment-added');
-                });
+                },
+                        function (error) {
+                            $scope.showLoadingSpinner = false;
+                            $ionicPopup.show({
+                                template: '',
+                                title: 'Loading experiments failed. Try again.',
+                                scope: $scope,
+                                buttons: [
+                                    {
+                                        text: 'OK',
+                                        type: 'button-calm'
+                                    }
+                                ]
+                            });
+                        });
             };
         });
 
